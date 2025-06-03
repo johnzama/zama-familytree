a21d5b12d64af94ffd197d29483c5a6e0e14486b
johnzama <johnsparta2121@gmail.com>
2025-06-03 19:21:09 -0400

    data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Tree from 'react-d3-tree';
+import treeData from './treeData';
+
+const containerStyles = {
+  width: '100vw',
+  height: '100vh',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyles}>
+      <Tree
+        data={treeData}
+        orientation="vertical"
+        translate={{ x: 300, y: 100 }}
+        pathFunc="step"
+      />
     </div>
   );
 }
